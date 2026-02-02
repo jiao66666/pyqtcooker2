ce5fe61a03081dacad5522cc78b2c6eaf2d7a9ff
jiaonewcook <jiaocook@126.com>
2026-02-02 11:47:50 +0800

    add four reset button ui

--- webcontrol.py
+++ webcontrol.py
@@ -157,4 +157,4 @@ def fixmotor():
            
 if __name__ == '__main__':
     # 绑定到所有网络接口，允许局域网访问
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
