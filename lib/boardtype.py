0138a4de44ac8de48b0ee3968e260aafabe8b6a4
jiaonewcook <jiaocook@126.com>
2026-02-27 13:05:50 +0800

    update 4 stop position level pos

--- lib/boardtype.py
+++ lib/boardtype.py
@@ -10,11 +10,11 @@ POT2_FLIP_MOTOR = 3  # 2号锅翻转电机编号
 
 
 POT1_POS_OUTFOOD_FLIP  = 10  # 1号锅外倒菜位,翻转位
-POT1_POS_OUTFOOD_LEVEL = 4.20  # 1号锅外倒菜位，水平位
+POT1_POS_OUTFOOD_LEVEL = 4.16  # 1号锅外倒菜位，水平位
 POT1_POS_INFOOD_FLIP  = 5.05  # 1号锅内倒菜位,翻转位
-POT1_POS_INFOOD_LEVEL = 4.20  # 1号锅内倒菜位，水平位
+POT1_POS_INFOOD_LEVEL = 4.16  # 1号锅内倒菜位，水平位
 POT1_POS_WASHPOT_FLIP  = -11.8  # 1号锅洗锅位,翻转位
-POT1_POS_WASHPOT_LEVEL = 4.20  # 1号锅洗锅位，水平位
+POT1_POS_WASHPOT_LEVEL = 4.16  # 1号锅洗锅位，水平位
 POT1_POS_FIREPOT_FLIP  = 0  # 1号锅烧菜位,翻转位
 POT1_POS_FIREPOT_LEVEL = 0  # 1号锅烧菜位，水平位
 
