8d11c0945139f0e275d2a77cae12d966aeb13899
jiaonewcook <jiaocook@126.com>
2026-02-28 11:03:35 +0800

    fix varspeed target

--- webcontrol.py
+++ webcontrol.py
@@ -709,7 +709,7 @@ def testvarspeedsingle():
 
     speed_params_val = parse_speed_params(speed_params)
     print(speed_params_val)
-    success = boardercontrollers["boardcontroller1"].motors[POT1_MOVE_MOTOR].gotask_advanced_speed(5.05,speed_params_val)  
+    success = boardercontrollers["boardcontroller1"].motors[POT1_MOVE_MOTOR].gotask_advanced_speed(4.16,speed_params_val)  
 
     print("**************************************1号变速运动测试结束******************************")
 
