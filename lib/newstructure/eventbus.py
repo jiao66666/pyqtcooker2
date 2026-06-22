5492e4569ef22e4f8895b68299901b567f7550ca
jiaonewcook <jiaocook@126.com>
2026-06-22 10:14:03 +0800

    add unscrip even to eventbus

--- lib/newstructure/eventbus.py
+++ lib/newstructure/eventbus.py
@@ -10,3 +10,7 @@ class EventBus:
     def publish(self, event_type, data=None):
         for cb in self.subscribers[event_type]:
             cb(data)
+
+    def unsubscribe(self, event_type, callback):
+        if callback in self.subscribers[event_type]:
+            self.subscribers[event_type].remove(callback)            
